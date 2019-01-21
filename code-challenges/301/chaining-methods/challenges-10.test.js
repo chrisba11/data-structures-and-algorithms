80aece2e75a677c5f2d5369d71ba64bf6dda6441
Chris Ball <chrisball7@gmail.com>
2019-01-21 06:28:53 -0800

    first 2 passing

--- code-challenges/301/chaining-methods/challenges-10.test.js
+++ code-challenges/301/chaining-methods/challenges-10.test.js
@@ -12,8 +12,13 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  let count = 0;
+  input.forEach(array => {
+    array.map(element => element === target ? count++ : element);
+  });
+  return count;
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -26,7 +31,11 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  let sum = 0;
+  input.forEach(array => {
+    sum = sum + array.reduce((accumulator, current) => accumulator + current);
+  });
+  return sum;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -42,7 +51,11 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  // const final = [];
+  console.log(input.forEach(array => {
+    return array.filter(num => num % 5 === 0);
+  }));
+
 };
 
 /* ------------------------------------------------------------------------------------------------
