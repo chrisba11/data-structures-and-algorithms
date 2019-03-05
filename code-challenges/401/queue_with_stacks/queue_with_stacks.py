d66f7499af4f14710569a21f53e19ea7b7ec8947
Chris Ball <chrisball7@gmail.com>
2019-03-05 07:25:26 -0800

    queue class written with init method

---
+++ code-challenges/401/queue_with_stacks/queue_with_stacks.py
@@ -0,0 +1,71 @@
+class Node():
+    """
+    Class to create a new node.
+    """
+    def __init__(self, data):
+        """
+        Initializes an instance of a node with a *data* attribute value equal to *data*.
+        """
+        self.data = data
+        self._next = None
+
+
+class Stack():
+    """
+    Method to generate and modify a stack.
+    """
+
+    top = None
+    empty = 'Empty Stack'
+
+    def push(self, new_data):
+        """
+        Method to add a new node at the top of a stack with a *data* attribute value equal to *new_data*.
+        Returns *data* value.
+        """
+        new_node = Node(new_data)
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node._next = self.top
+            self.top = new_node
+        return self.top.data
+
+    def pop(self):
+        """
+        Method to remove the top node from a stack and return the value of its *data* attribute.
+        """
+        if self.top is not None:
+            current = self.top
+            self.top = self.top._next
+            current._next = None
+            return current.data
+        else:
+            return self.empty
+
+    def peek(self):
+        """
+        Method to return the value of the *data* attribute from the top node of a stack.
+        """
+        if self.top is not None:
+            return self.top.data
+        else:
+            return self.empty
+
+
+class Queue():
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        stack_one = Stack()
+        stack_two = Stack()
+
+    def enqueue(self, data):
+        """
+
+        """
+        
