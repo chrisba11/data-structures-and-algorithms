a8dca3d26cf99e57bf1811268baba5ba8552dde0
Chris Ball <chrisball7@gmail.com>
2019-04-02 12:37:06 -0700

    re-starting using slice arrays

--- code-challenges/cf401/merge_sort/merge_sort.py
+++ code-challenges/cf401/merge_sort/merge_sort.py
@@ -6,17 +6,22 @@ def merge_sort(list):
         """
         Adjusts the starting and ending point of the array to narrow down the number of elements to sort.
         """
-        f1 = front
-        e1 = ((len(list) - f1) // 2) + f1
-        f2 = e1 + 1 if e1 != len(list) - 1 else None
-        e2 = len(list) - 1 if f2 is not None else None
+        
 
-        if end - front > 1:
-            end = (end - front) // 2
-            _divide(front, end)
-        if list[end] > list[front]:
-            curr_end = end
-            list[end], list[front] = list[front], list[end]
+
+
+
+        # f1 = front
+        # e1 = ((len(list) - f1) // 2) + f1
+        # f2 = e1 + 1 if e1 != len(list) - 1 else None
+        # e2 = len(list) - 1 if f2 is not None else None
+
+        # if end - front > 1:
+        #     end = (end - front) // 2
+        #     _divide(front, end)
+        # if list[end] > list[front]:
+        #     curr_end = end
+        #     list[end], list[front] = list[front], list[end]
 
 
 
