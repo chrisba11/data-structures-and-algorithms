31ff084296a8fa8bd8552829c98c3b7237ea08f5
Chris Ball <chrisball7@gmail.com>
2019-04-02 08:32:22 -0700

    still working on algorithm

--- code-challenges/cf401/merge_sort/merge_sort.py
+++ code-challenges/cf401/merge_sort/merge_sort.py
@@ -1,19 +1,19 @@
 def merge_sort(list):
     """
-
+    Takes in a list and sorts it using merge sort logic. Returns the list.
     """
-    f1 = 0
-    e1 = ((len(list) - f1) // 2) + f1
-    f2 = e1 + 1 if e1 != len(list) - 1 else None
-    e2 = len(list) - 1 if f2 is not None else None
+    def _divide(list, front=0):
+        """
+        Adjusts the starting and ending point of the array to narrow down the number of elements to sort.
+        """
+        f1 = front
+        e1 = ((len(list) - f1) // 2) + f1
+        f2 = e1 + 1 if e1 != len(list) - 1 else None
+        e2 = len(list) - 1 if f2 is not None else None
 
-    def _divide(front, end, list):
         if end - front > 1:
             end = (end - front) // 2
             _divide(front, end)
         if list[end] > list[front]:
             curr_end = end
             list[end], list[front] = list[front], list[end]
-
-
-# 1. length = 3 | list[3] - list[] | list[] - list[]
