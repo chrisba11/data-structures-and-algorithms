1a993a814ed373a2e4fe78ef81cdf17432dacf8a
Chris Ball <chrisball7@gmail.com>
2019-04-03 09:07:24 -0700

    re-wrote function based on review of content in class and whiteboarding

--- code-challenges/cf401/merge_sort/merge_sort.py
+++ code-challenges/cf401/merge_sort/merge_sort.py
@@ -2,58 +2,35 @@ def merge_sort(list):
     """
     Takes in a list and sorts it using merge sort logic. Returns the list.
     """
-    def _divide(list, front=0):
+    if len(list) <= 1:
+        return list
+
+    mid = len(list) // 2
+    left_slice = list[:mid]
+    right_slice = list[mid:]
+
+    left = merge_sort(left_slice)
+    right = merge_sort(right_slice)
+
+    def _merge_sort(list_one, list_two):
         """
-        Adjusts the starting and ending point of the array to narrow down the number of elements to sort.
+        Merges two lists together, sorting them as it assembles a new list and then returns the new list.
         """
-        
+        output = []
+        count_one = 0
+        count_two = 0
 
+        while count_one < len(list_one) and count_two < len(count_two):
+            if list_one[count_one] < list_two[count_two]:
+                output.append(list_one[count_one])
+                count_one += 1
+            else:
+                output.append(list_two[count_two])
+                count_two += 1
 
+        output += list_one[count_one:]
+        output += list_two[count_two:]
 
+        return output
 
-        # f1 = front
-        # e1 = ((len(list) - f1) // 2) + f1
-        # f2 = e1 + 1 if e1 != len(list) - 1 else None
-        # e2 = len(list) - 1 if f2 is not None else None
-
-        # if end - front > 1:
-        #     end = (end - front) // 2
-        #     _divide(front, end)
-        # if list[end] > list[front]:
-        #     curr_end = end
-        #     list[end], list[front] = list[front], list[end]
-
-
-
-
-
-# def merge(left, right):
-#     """Merge sort merging function."""
-
-#     left_index, right_index = 0, 0
-#     result = []
-#     while left_index < len(left) and right_index < len(right):
-#         if left[left_index] < right[right_index]:
-#             result.append(left[left_index])
-#             left_index += 1
-#         else:
-#             result.append(right[right_index])
-#             right_index += 1
-
-#     result += left[left_index:]
-#     result += right[right_index:]
-#     return result
-
-
-# def merge_sort(array):
-#     """Merge sort algorithm implementation."""
-
-#     if len(array) <= 1:  # base case
-#         return array
-
-#     # divide array in half and merge sort recursively
-#     half = len(array) // 2
-#     left = merge_sort(array[:half])
-#     right = merge_sort(array[half:])
-
-#     return merge(left, right)
+    return _merge_sort(left, right)
