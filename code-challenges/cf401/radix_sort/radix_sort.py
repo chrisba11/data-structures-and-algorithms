7468cc3954b4109e0537608689a2b94f20449a1f
Chris Ball <chrisball7@gmail.com>
2019-04-03 15:47:46 -0700

    re-thinking some things, but stuck in a loop somewhere

--- code-challenges/cf401/radix_sort/radix_sort.py
+++ code-challenges/cf401/radix_sort/radix_sort.py
@@ -3,4 +3,31 @@ from ..stacks_and_queues.stacks_and_queues import Queue
 
 def radix_sort(list):
     queues = [Queue() for i in range(10)]
-    print(queues)
+    max_val = max(list)
+    iterations = len(str(max_val)) + 1
+    loop_num = 1
+
+    def _radix_sort(list):
+        nonlocal iterations
+        nonlocal loop_num
+
+        while loop_num < iterations:
+            output = []
+
+            for num in list:
+                q = num % (10 ** loop_num)
+                queues[q].enqueue(num)
+
+            for queue in queues:
+                while queue.front:
+                    output.append(queue.dequeue)
+
+            loop_num += 1
+
+            print(output)
+
+            return _radix_sort(output)
+
+        return list
+
+    return _radix_sort(list)
