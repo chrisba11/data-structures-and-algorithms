ce2cb63ced688eb65019585f3cb782e990afa2e3
Chris Ball <chrisball7@gmail.com>
2019-04-09 21:16:47 -0700

    passing 7th test

--- code-challenges/cf401/breadth_first/test_breadth_first.py
+++ code-challenges/cf401/breadth_first/test_breadth_first.py
@@ -41,3 +41,23 @@ def test_three_vertices_with_edge_between_two():
     g.add_edge('smoke', 'mirrors')
     assert breadth_first(g, 'mirrors') == ['mirrors', 'smoke']
     assert breadth_first(g, 'burgers') == ['burgers']
+
+
+def test_several_vertices_with_edges():
+    g = Graph()
+    g.add_vertex('smoke')
+    g.add_vertex('mirrors')
+    g.add_vertex('burgers')
+    g.add_vertex('hot dog')
+    g.add_vertex('potato')
+    g.add_vertex('llama')
+    g.add_vertex('pickle')
+    g.add_edge('smoke', 'mirrors')
+    g.add_edge('smoke', 'burgers')
+    g.add_edge('smoke', 'hot dog')
+    g.add_edge('potato', 'burgers')
+    g.add_edge('potato', 'llama')
+    g.add_edge('pickle', 'burgers')
+    g.add_edge('llama', 'hot dog')
+    g.add_edge('pickle', 'mirrors')
+    assert breadth_first(g, 'burgers') == ['burgers', 'smoke', 'potato', 'pickle', 'mirrors', 'hot dog', 'llama']
