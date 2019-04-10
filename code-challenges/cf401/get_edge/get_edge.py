6a7c2b8beaca1dcc25ee6e5efd16e1042b99e56a
Chris Ball <chrisball7@gmail.com>
2019-04-10 09:03:02 -0700

    passing first two tests, adjusted to return False for 0 or 1 items in list

--- code-challenges/cf401/get_edge/get_edge.py
+++ code-challenges/cf401/get_edge/get_edge.py
@@ -2,4 +2,18 @@ from ..graph.graph import Graph
 
 
 def get_edge(graph, list):
-    
+    price = 0
+
+    if len(list) < 2:
+        return (False, 0)
+
+    for i in range(1, len(list)-1):
+        neighbors_of_previous = graph.get_neighbors(list[i-1])
+        current = list[i]
+
+        if current in neighbors_of_previous:
+            price += neighbors_of_previous[current]
+        else:
+            return (False, 0)
+
+    return (True, price)
--- code-challenges/cf401/get_edge/test_get_edge.py
+++ code-challenges/cf401/get_edge/test_get_edge.py
@@ -12,12 +12,48 @@ def test_works_with_empty_list():
     g.add_vertex('Sydney')
     g.add_vertex('Melbourne')
     g.add_edge('Sydney', 'Melbourne')
-    assert get_edge(g, []) is (False, 0)
+    assert get_edge(g, []) == (False, 0)
 
 
+@pytest.mark.skip
 def test_works_with_single_item_in_list():
     g = Graph()
     g.add_vertex('Sydney')
     g.add_vertex('Melbourne')
-    g.add_edge('Sydney', 'Melbourne')
-    assert get_edge(g, ['Sydney']) is (False, 0)
+    g.add_edge('Sydney', 'Melbourne', 150)
+    assert get_edge(g, ['Sydney']) == (False, 1)
+
+
+@pytest.mark.skip
+def test_works_with_two_list_items():
+    g = Graph()
+    g.add_vertex('Sydney')
+    g.add_vertex('Melbourne')
+    g.add_edge('Sydney', 'Melbourne', 150)
+    assert get_edge(g, ['Sydney, Melbourne']) == (True, 151)
+
+
+@pytest.mark.skip
+def test_works_with_multiple_list_items_false():
+    g = Graph()
+    g.add_vertex('Sydney')
+    g.add_vertex('Melbourne')
+    g.add_vertex('London')
+    g.add_vertex('New York')
+    g.add_edge('Sydney', 'Melbourne', 150)
+    g.add_edge('London', 'Melbourne', 800)
+    g.add_edge('London', 'New York', 600)
+    assert get_edge(g, ['Sydney, Melbourne', 'New York']) == (False, 1)
+
+
+@pytest.mark.skip
+def test_works_with_multiple_list_items_true():
+    g = Graph()
+    g.add_vertex('Sydney')
+    g.add_vertex('Melbourne')
+    g.add_vertex('London')
+    g.add_vertex('New York')
+    g.add_edge('Sydney', 'Melbourne', 150)
+    g.add_edge('London', 'Melbourne', 800)
+    g.add_edge('London', 'New York', 600)
+    assert get_edge(g, ['Sydney, Melbourne', 'London', 'New York']) == (True, 1551)
