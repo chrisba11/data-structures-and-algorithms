fa3db2ecdc1487aad02367a94525e37a1b128c2f
Chris Ball <chrisball7@gmail.com>
2019-04-11 16:06:57 -0700

    converted breadth_first to a method on an extension of the Graph class

--- code-challenges/cf401/breadth_first/test_breadth_first.py
+++ code-challenges/cf401/breadth_first/test_breadth_first.py
@@ -1,50 +1,49 @@
-from ..graph.graph import Graph
-from .breadth_first import breadth_first
+from .breadth_first import BreadthFirstGraph
 import pytest
 
 
 def test_exists():
-    assert breadth_first
+    assert BreadthFirstGraph().breadth_first
 
 
 def test_empty_graph():
-    g = Graph()
-    assert breadth_first(g, 'smoke') == []
+    g = BreadthFirstGraph()
+    assert g.breadth_first('smoke') == []
 
 
 def test_one_vertex():
-    g = Graph()
+    g = BreadthFirstGraph()
     g.add_vertex('smoke')
-    assert breadth_first(g, 'smoke') == ['smoke']
+    assert g.breadth_first('smoke') == ['smoke']
 
 
 def test_two_vertices_no_edge():
-    g = Graph()
+    g = BreadthFirstGraph()
     g.add_vertex('smoke')
     g.add_vertex('mirrors')
-    assert breadth_first(g, 'smoke') == ['smoke']
+    assert g.breadth_first('smoke') == ['smoke']
 
 
 def test_two_vertices_with_edge():
-    g = Graph()
+    g = BreadthFirstGraph()
     g.add_vertex('smoke')
     g.add_vertex('mirrors')
     g.add_edge('smoke', 'mirrors')
-    assert breadth_first(g, 'smoke') == ['smoke', 'mirrors']
+    assert g.breadth_first('smoke') == ['smoke', 'mirrors']
 
 
 def test_three_vertices_with_edge_between_two():
-    g = Graph()
+    g = BreadthFirstGraph()
     g.add_vertex('smoke')
     g.add_vertex('mirrors')
     g.add_vertex('burgers')
     g.add_edge('smoke', 'mirrors')
-    assert breadth_first(g, 'mirrors') == ['mirrors', 'smoke']
-    assert breadth_first(g, 'burgers') == ['burgers']
+    assert g.breadth_first('mirrors') == ['mirrors', 'smoke']
+    assert g.breadth_first('burgers') == ['burgers']
 
 
 def test_several_vertices_with_edges():
-    g = Graph()
+    g = BreadthFirstGraph()
     g.add_vertex('smoke')
     g.add_vertex('mirrors')
     g.add_vertex('burgers')
@@ -60,4 +59,12 @@ def test_several_vertices_with_edges():
     g.add_edge('pickle', 'burgers')
     g.add_edge('llama', 'hot dog')
     g.add_edge('pickle', 'mirrors')
-    assert breadth_first(g, 'burgers') == ['burgers', 'smoke', 'potato', 'pickle', 'mirrors', 'hot dog', 'llama']
+    assert g.breadth_first('burgers') == [
+        'burgers',
+        'smoke',
+        'potato',
+        'pickle',
+        'mirrors',
+        'hot dog',
+        'llama'
+        ]
